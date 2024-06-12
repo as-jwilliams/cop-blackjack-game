928bdb17a7022ff1dfa17ef59ea505f103053546
as-jwilliams <134523416+as-jwilliams@users.noreply.github.com>
2024-06-12 11:13:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "aws_iam_role" "lambda_exec" {
       Version = "2012-10-17",
       Statement = [
         {
-          Action = [
+          Action: [
             "logs:CreateLogGroup",
             "logs:CreateLogStream",
             "logs:PutLogEvents"
