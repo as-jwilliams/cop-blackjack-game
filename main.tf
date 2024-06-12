51519dab44c50923e5ee97fa356ec1ca883c8385
as-jwilliams <134523416+as-jwilliams@users.noreply.github.com>
2024-06-12 11:42:33 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_iam_role" "existing_lambda_exec" {
+  name = "lambda_exec_role"
+}
+
 data "aws_s3_bucket" "existing_bucket" {
   bucket = "blackjack-game-site"
 }
@@ -36,55 +40,10 @@ resource "aws_cloudfront_distribution" "cdn" {
   }
 }
 
-resource "aws_iam_role" "lambda_exec" {
-  name = "lambda_exec_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Sid    = "",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy" "lambda_exec_policy" {
-  name = "lambda_exec_policy"
-  role = aws_iam_role.lambda_exec.id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action: [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Effect   = "Allow",
-        Resource = "arn:aws:logs:*:*:*"
-      },
-      {
-        Action: [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:ListBucket"
-        ],
-        Effect: "Allow",
-        Resource: "*"
-      }
-    ]
-  })
-}
-
 resource "aws_lambda_function" "blackjack_game" {
   filename         = "blackjack_game.zip"
   function_name    = "blackjack_game"
-  role             = aws_iam_role.lambda_exec.arn
+  role             = data.aws_iam_role.existing_lambda_exec.arn
   handler          = "blackjack.handler"
   runtime          = "python3.8"
 }
