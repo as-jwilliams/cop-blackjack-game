4e44a6b8717469719f99018a2df1487576fb02d7
as-jwilliams <134523416+as-jwilliams@users.noreply.github.com>
2024-06-12 09:57:05 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,69 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "static_site" {
+  bucket = "blackjack-game-site"
+  website {
+    index_document = "index.html"
+  }
+}
+
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name = aws_s3_bucket.static_site.bucket_regional_domain_name
+    origin_id   = "S3-blackjack-game-site"
+  }
+  enabled = true
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-blackjack-game-site"
+    viewer_protocol_policy = "redirect-to-https"
+  }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_lambda_function" "blackjack_game" {
+  filename         = "blackjack_game.zip"
+  function_name    = "blackjack_game"
+  role             = aws_iam_role.lambda_exec.arn
+  handler          = "blackjack.handler"
+  runtime          = "python3.8"
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Sid    = "",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+  inline_policy {
+    name = "lambda_policy"
+    policy = jsonencode({
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Action = [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ],
+          Effect   = "Allow",
+          Resource = "arn:aws:logs:*:*:*"
+        }
+      ]
+    })
+  }
+}
